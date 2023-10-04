4e11004f8f9c75d691e1083498ccc39af055cea9
vishalS23 <95996521+vishalS23@users.noreply.github.com>
2023-10-04 21:57:34 +0530

    Day-1.js

---
+++ Day-1-js/Day-1.js
@@ -0,0 +1,19 @@
+var name = "Vishal";
+var MobNo = "1234567890";
+var University = "SPPU";
+var branch = "IT";
+var year = "2023";
+
+ console.log(name)
+ console.log(MobNo)
+ console.log(University)
+ console.log(branch)
+ console.log(year)
+
+ var v = 23;
+ var p = 7;
+
+ console.log("Addition:"+(v+p))
+ console.log("Subtraction:"+(v-p))
+ console.log("Multiplication:"+(v*p))
+ console.log("Division:"+(v/p))
