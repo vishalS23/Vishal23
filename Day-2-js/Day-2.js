705ee602cc11698b1f5caae21351bdbc5c1f3d03
vishalS23 <95996521+vishalS23@users.noreply.github.com>
2023-10-05 22:40:16 +0530

    Day-2.js

---
+++ Day-2-js/Day-2.js
@@ -0,0 +1,16 @@
+var percentage = 90
+
+if(percentage >80){
+    console.log ("yor are eligible")
+
+}else if(percentage >70 && percentage <80) {
+    console.log ("wait for second round")
+
+}else if(percentage >60 || percentage ==60){
+    console.log ("wait for third round")
+
+}else{
+    console.log ("you are not eligible")
+}
+
+
