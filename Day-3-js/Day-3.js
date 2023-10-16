3e89fdf848e124695816861649c81b0927a47670
vishalS23 <95996521+vishalS23@users.noreply.github.com>
2023-10-16 08:12:30 +0530

    Update Day-3.js

--- Day-3-js/Day-3.js
+++ Day-3-js/Day-3.js
@@ -7,3 +7,22 @@ if(number%2==0){
 
     console.log(`${number} is odd`)
 }
+
+var number = 22;
+var number2 = 78;
+var number3 = 67;
+
+if (number % 2 == 0) {
+    if (number % 2 == 0) {
+        console.log(`${number} and ${number2} are even numbers`)
+    
+    }else{
+        console.log(`${number} is even but ${number2} is odd`)
+    }
+}else{
+    if (number3 % 2 == 0) {
+        console.log(`${number3}is even and ${number} is odd`)
+    }else{
+        console.log(`${number3}and ${number} both are odd`)
+    }
+}
