b37579cf9182cb97c7f3f312eacc464eb825f550
vishalS23 <95996521+vishalS23@users.noreply.github.com>
2023-10-16 14:05:18 +0530

    Day-4.js

--- Day-4-js/Day-4.js
+++ Day-4-js/Day-4.js
@@ -1,3 +1,16 @@
-for (i = 0; i >= 10; i++){
+for(i=10; i > 0; i-- ){
     console.log(i)
 }
+
+function Maxnumber(a,b,c,d){
+    if(a<b && a<c && a<d){
+        console.log("a is small")
+    }else if(b<c && b<d){
+        console.log("b is small")
+    }else if(c<d){
+        console.log("c is small")
+    }else{
+        console.log("d is max")
+    }
+}
+Maxnumber(11,22,33,44)
