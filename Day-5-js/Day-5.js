ded9eb851860fe83f700ae2a4b01e66e7dafc38e
vishalS23 <95996521+vishalS23@users.noreply.github.com>
2023-10-16 15:23:37 +0530

    Day-5.js

--- Day-5-js/Day-5.js
+++ Day-5-js/Day-5.js
@@ -1,4 +1,10 @@
-const names = ["Vishal","Rushi","Rahul","Gaurav"]
+var Students=['Vishal','Rushi','Adi','Amit','Jin'];
+for(i=0; i<Students.length; i++){
+    console.log(Students[i])
+}
 
-const rollno =["23","24","25","26"]
-
+var mobile=['samsung','apple','oppo','vivo'];
+console.log(mobile[0])
+console.log(mobile[1])
+console.log(mobile[3])
+console.log(mobile[3])
